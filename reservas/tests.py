7e976f4fe24f8bec458f6002134e0e1427d3ccf0
Rafael Henrique Carneiro <rafael.h@outlook.com>
2025-11-23 22:58:16 -0300

    Adiciona testes unitários para validação de conflito

--- reservas/tests.py
+++ reservas/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Room, Reservation
+from datetime import date, time
+from django.core.exceptions import ValidationError
+
 
 # Create your tests here.
+class ReservationModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="tester")
+
+        self.room = Room.objects.create(
+            name="Lab test",
+            capacity=30,
+            type="LAB",
+        )
+
+        self.reservation = Reservation.objects.create(
+            room=self.room,
+            user=self.user,
+            date=date(2025, 12, 1),
+            start=time(14, 0),
+            end=time(16, 0),
+        )
+
+    def test_bloqueia_conflito_exato(self):
+        with self.assertRaises(ValidationError):
+            Reservation.objects.create(
+                room=self.room,
+                user=self.user,
+                date=date(2025, 12, 1),
+                start=time(14, 0),
+                end=time(16, 0),
+            )
+
+    def test_bloqueia_sobreposicao_parcial(self):
+        with self.assertRaises(ValidationError):
+            Reservation.objects.create(
+                room=self.room,
+                user=self.user,
+                date=date(2025, 12, 1),
+                start=time(15, 0),
+                end=time(17, 0),
+            )
+
+    def test_permite_horario_livre(self):
+        reservation_ok = Reservation.objects.create(
+            room=self.room,
+            user=self.user,
+            date=date(2025, 12, 1),
+            start=time(16, 0),
+            end=time(18, 0),
+        )
+
+        try:
+            reservation_ok.clean()
+
+        except ValidationError:
+            self.fail("O teste falhou: Deveria permitir agendamento sem conflito.")
